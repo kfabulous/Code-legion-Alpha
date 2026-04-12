36280976d0258e17817a36fdac31fa501ad6a468
ALPHA <fabulouskhing@gmail.com>
2026-04-12 13:02:33 +0000

    Add files via upload

---
+++ CPP Assignment/Day1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  string name;
+  int age;
+
+  cout << "Enter your name: ";
+  cin >> name;
+
+  cout << "Enter your age: ";
+  cin >> age;
+
+  cout<< "Hey " << name << " you are " << age << "years old!";
+
+  return (0);
+
+}
